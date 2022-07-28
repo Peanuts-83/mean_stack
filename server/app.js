9991561a1d5826a8d8da9ed1bcfe3312aeeeb865
Peanuts-83 <tranque@free.fr>
2022-07-28 09:51:30 +0200

    full app

---
+++ server/app.js
@@ -0,0 +1,36 @@
+const express = require('express');
+const path = require('path');
+const logger = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+require('dotenv').config()
+
+const db = {
+  userName: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE
+}
+
+const app = express();
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, '../public')));
+
+mongoose.connect(`mongodb+srv://${db.userName}:${db.password}@${db.database}?retryWrites=true&w=majority`,
+  function (err) {
+    if (err) {
+      console.log('Error', err);
+    } else {
+      console.log('Connected to mongoDB');
+    }
+  })
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+module.exports = app;
